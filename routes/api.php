dc4fdf195992e2afc8ffb331541dbdd9b0a2d7d7
kevin jose <kevin.jose4u@gmail.com>
2023-02-25 22:54:26 +0530

    updated category api2

--- routes/api.php
+++ routes/api.php
@@ -46,8 +46,7 @@ Route::prefix('vendor')->group(function () {
 Route::prefix('customer')->group(function () { 
     Route::post('/login', [ApiController::class, 'customerLogin'])->name('customerLogin');
     Route::post('/registration', [ApiController::class, 'customerRegistration'])->name('customerRegistration');
-    Route::post('/flyers', [ApiController::class, 'getCustomerFlyers'])->middleware(['auth:api']);
-    Route::post('/flyers/details', [ApiController::class, 'getCustomerFlyersDetails'])->middleware(['auth:api']);
+    Route::post('/category/shop', [ApiController::class, 'getCategoryList'])->middleware(['auth:api']);
     Route::get('/details', [ApiController::class, 'getCustomerDetails'])->middleware(['auth:api']);
     Route::get('/sliders', [ApiController::class, 'getSliders'])->middleware(['auth:api']);
     
