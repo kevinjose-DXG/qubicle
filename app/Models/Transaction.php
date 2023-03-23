192c70bc51158b89a4656450f936316697c923f6
kevin jose <kevin.jose4u@gmail.com>
2023-03-23 13:11:24 +0530

    updated transaction

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -8,13 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     use HasFactory;
-
-    public function userDetail()
-    {
-        return $this->hasOne(User::class,'id','vendor_id');
-    }
-    public function planDetail()
-    {
-        return $this->hasOne(VendorPlan::class,'id','plan_id');
-    }
 }
