8e598f368a3e28955ccfadf4937e6daf94fcdf1a
kevin jose <kevin.jose4u@gmail.com>
2023-04-01 09:14:17 +0530

    updated combo offers details table

--- database/migrations/2023_03_16_220915_create_combo_offers_table.php
+++ database/migrations/2023_03_16_220915_create_combo_offers_table.php
@@ -15,6 +15,11 @@ class CreateComboOffersTable extends Migration
     {
         Schema::create('combo_offers', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->nullable();
+            $table->text('image')->nullable();
+            $table->text('description')->nullable();
+            $table->float('price',8,2)->default('0.00');
+            $table->enum('status',['active','inactive'])->default('active');
             $table->timestamps();
         });
     }
